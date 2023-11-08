3d94ca178cc7d69569d5a7544c6201584de5d1a7
ramesh banakar <ramesh.s.b@campusuvce.in>
2023-11-09 03:00:34 +0530

    web server

--- basics/03_http/01_webserver.js
+++ basics/03_http/01_webserver.js
@@ -1,6 +1,8 @@
 const http=require("http")
+const fs=require("fs")
+data = fs.readFileSync("../textFIle/index.html","utf-8");
 const server=http.createServer((req,res)=>{
     console.log("new request came")
-    res.end("hello from server...!!!")
+    res.end(data);
 })
 server.listen(8000,"localhost",()=>{console.log("server has started")})
