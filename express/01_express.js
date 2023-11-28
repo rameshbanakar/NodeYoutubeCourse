512f651c4ccaf2f5cd3cbc4647d45144cdaaa78b
ramesh banakar <ramesh.s.b@campusuvce.in>
2023-11-28 08:15:41 +0530

    params adding to url

--- express/01_express.js
+++ express/01_express.js
@@ -1,8 +1,33 @@
-const express=require("express")
-const data=require("./movies.json")
+const express = require("express");
+const data = require("./movies.json");
 
-const app=express()
-app.get("/api/movies/",(req,res)=>{
-    res.json(data);
-})
-app.listen(8000,()=>{console.log("server started@8000")})
+const app = express();
+app.use(express.json())
+app.get("/api/movies/", (req, res) => {
+  res.json(data);
+});
+
+app.get("/api/movies/:id/:name?", (req, res) => {
+  let id=parseInt(req.params.id)
+  console.log(id)
+  let movie=data.find(el=>el.id===id)
+  if(!movie){
+    res.status(404).send("data not found")
+  }
+  console.log(movie)
+  res.json(movie);
+});
+
+
+app.post("/api/movies/", (req, res) => {
+  console.log(req.body);
+  const newId=data[data.length-1].id+1
+  const newObj=Object.assign({id:newId},req.body)
+  console.log(newObj)
+  data.push(newObj)
+  res.send("created")
+});
+app.listen(8000, () => {
+  console.log("server started@8000");
+  
+});
